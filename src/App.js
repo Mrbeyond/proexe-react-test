7cb2df88f6a193511f876518f4dfd097e6288508
Beyond <beyondbeyond602@gmail.com>
2022-02-06 17:42:14 +0100

    Added netlify.tolm file for page redirect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import UserList from './Components/UserList';
+import { mock } from "./mockdata";
 import {
   BrowserRouter as Router,
   Routes,
@@ -19,11 +20,19 @@ const PROXY = "https://my-json-server.typicode.com/karolkproexe/jsonplaceholderd
 
 function App({setUsers, all_users}) {
 
+  const [apiError, setApiError] = useState('');
+
   useEffect(()=>{
     if(all_users) return;
     axios.get(PROXY).then(data=>{
       setUsers(data.data)
     }).catch(error=>{
+      // if error load data locally
+      setUsers(mock);
+      setApiError("Something wrong, it seems to be network errror, now using local data.");
+      setTimeout(()=>{
+        setApiError("")
+      },4000);
       // console.log(error);
     })
   },[setUsers, all_users]);
@@ -32,6 +41,12 @@ function App({setUsers, all_users}) {
     <div className="px-2 pb-10 sm:px-10 md:px-20 lg:px-28 xl:px-36">
       <Router>
         <h1 className="dashboard-text">Dashboard</h1>
+        {apiError &&
+          <div 
+            className="text-sm text-red-400 mb-5 mt-3 flex justify-center" >
+              {apiError}
+          </div>
+        }
         <Routes>
           <Route exact path="/" element={<UserList all_users={all_users} />} /> 
           <Route exact path="/add-user" element={<UserForm />} />
